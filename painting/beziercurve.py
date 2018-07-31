63a86a24f526d6c8e6253462c3196512da0c3e61
Trevor <rowdeil@gmail.com>
2018-07-30 22:34:08 -0700

    Added more examples of using the painting functionality

---
+++ painting/beziercurve.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial
+
+This program draws a Bezier curve with QPainterPath
+
+Author: Jan Bodnar
+Website: zetcode.com
+Last Edited: July 2018
+Edited By: Trevor O'Neil
+"""
+
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QPainterPath
+from PyQt5.QtCore import Qt
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 380, 250)
+        self.setWindowTitle('Bezier Curve')
+        self.show()
+
+    def paintEvent(self, e):
+
+        qp = QPainter()
+        qp.begin(self)
+        qp.setRenderHint(QPainter.Antialiasing)
+        self.drawBezierCurve(qp)
+        qp.end()
+
+    def drawBezierCurve(self, qp):
+
+        path = QPainterPath()
+        path.moveTo(30, 30)
+        path.cubicTo(30, 30, 200, 350, 350, 30)
+
+        qp.drawPath(path)
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
